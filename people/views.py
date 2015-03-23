0547dbb24f571da2d15601b16f0d7d3573177fd6
KasiaKasia <bulicka.alexandra@gmail.com>
2015-03-24 00:16:39 +0100

    Update views.py

--- people/views.py
+++ people/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from people.models import Person
 from django.core.urlresolvers import reverse
